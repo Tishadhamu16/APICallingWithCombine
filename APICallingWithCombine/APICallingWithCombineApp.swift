83fb2c5619a16289dbc23d9051fd9dfe63ce2218
Tishadhamu16 <tdhamu1601@gmail.com>
2025-02-15 19:12:08 +0530

    Initial Commit

---
+++ APICallingWithCombine/APICallingWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallingWithCombineApp.swift
+//  APICallingWithCombine
+//
+//  Created by Tisha Dhamu on 15/02/25.
+//
+
+import SwiftUI
+
+@main
+struct APICallingWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
